a6d856e3d73518f260428ef8b982d26fcb528d00
PhuongNgo2308 <39096015+PhuongNgo2308@users.noreply.github.com>
2021-07-03 17:28:56 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h3>Hello world</h3>
+        <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import "./assets/scss/app.scss";
+
+export default {
+  name: "App",
+  components: {},
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// src/main.js
+
+import Vue from "vue";
+import vuetify from "@/plugins/vuetify"; // path to vuetify export
+import App from "@/App";
+
+new Vue({
+  vuetify,
+  render: (h) => h(App)
+}).$mount("#app");
